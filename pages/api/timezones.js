ee945ce78dd42df8eb0b81407a392da4081860f4
Santiago Lorenzo <santylore4@gmail.com>
2021-03-30 11:36:17 -0300

    http endopoint changed to https

--- pages/api/timezones.js
+++ pages/api/timezones.js
@@ -1,9 +1,9 @@
 export async function getAllTimezones() {
-  const timezones = await fetch('http://worldtimeapi.org/api/timezone')
+  const timezones = await fetch('https://worldtimeapi.org/api/timezone')
   return await timezones.json()
 }
 
 export async function getTimezone(path) {
-  const timezones = await fetch(`http://worldtimeapi.org/api/timezone/${path}`)
+  const timezones = await fetch(`https://worldtimeapi.org/api/timezone/${path}`)
   return await timezones.json()
 }
